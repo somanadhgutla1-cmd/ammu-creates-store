878a0d46afa574531eaa524095c308703401f6a0
Somanadh <somanadhgutla1@gmail.com>
2026-09-07 19:43:26 +0530

    Fix Supabase Client Key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://xrsnfqielfdywwljemik.supabase.co'
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyc25mcWllbGZkeXd3bGplbWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDg6MDQ0MDA3M30.BglAXrOH4aQ78IBr_OTP5LpJSmXrSU8BLeP_SizoHQ4'
+const supabaseUrl = 'https://xrsnfqielfdywwljemik.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhyc25mcWllbGZkeXd3bGplbWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg2MDg6MDQ0MDA3M30.BglAXrOH4aQ78IBr_OTP5LpJSmXrSU8BLeP_SizoHQ4';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
